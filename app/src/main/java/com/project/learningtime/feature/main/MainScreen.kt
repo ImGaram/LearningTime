0ac3743f9f6d68121cb463937fa9266c9b8dfd5a
ImGaram <gim78014@gmail.com>
2025-02-23 20:49:21 +0900

    :sparkles: :: 1차 구현할 screen 생성

---
+++ app/src/main/java/com/project/learningtime/feature/main/MainScreen.kt
@@ -0,0 +1,15 @@
+package com.project.learningtime.feature.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainScreen() {
+
+}
+
+@Preview
+@Composable
+private fun MainScreenPreview() {
+    MainScreen()
+}
---
+++ app/src/main/java/com/project/learningtime/feature/timer/TimerScreen.kt
@@ -0,0 +1,15 @@
+package com.project.learningtime.feature.timer
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TimerScreen() {
+
+}
+
+@Preview
+@Composable
+private fun TimerScreenPreview() {
+    TimerScreen()
+}
