b35f4bdcdfe8119f04e654cab0ae63feaa954836
ImGaram <gim78014@gmail.com>
2025-03-10 23:04:36 +0900

    :wrench: :: gradle custom plugin 등록

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = libs.plugins.learningtime.android.application.get().pluginId
+            implementationClass = "AndroidApplicationPlugin"
+        }
+        register("androidLibrary") {
+            id = libs.plugins.learningtime.android.library.get().pluginId
+            implementationClass = "AndroidLibraryPlugin"
+        }
+        register("compose") {
+            id = libs.plugins.learningtime.compose.get().pluginId
+            implementationClass = "ComposePlugin"
+        }
+        register("hilt") {
+            id = libs.plugins.learningtime.hilt.get().pluginId
+            implementationClass = "HiltPlugin"
+        }
+        register("room") {
+            id = libs.plugins.learningtime.room.get().pluginId
+            implementationClass = "RoomPlugin"
+        }
+    }
+}
